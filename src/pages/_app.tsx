2cedde051792ae53615cc265cab7e775d00e8564
Miguel <buguel2000@gmail.com>
2021-02-26 09:25:36 -0300

    adicionando-modal-e-finalisando

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,9 @@
 // ESSE ARQUIVO FICAR ESTATICO POR VOLTA DA APLICAÇÃO EX:HEADER, SIDEBAR.
 import '../styles/global.css'
 
-import { ChallengesProvider } from '../contexts/ChallengesContext'
-
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
